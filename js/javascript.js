df14eac316c024de370e20c9df1d6d652a8141d7
SergioGarzon <sggarzonutn@gmail.com>
2024-06-26 19:08:56 -0300

    New commit

--- js/javascript.js
+++ js/javascript.js
@@ -2,22 +2,28 @@ class Escena extends Phaser.Scene {
 
     preload() {
         this.load.image('fondo', '../Images/rocket.png');
+        this.load.audio('sonido', ['../sounds/navesound.mp3']);  
+        //this.input.setDefaultCursor(url(''), pointer);      
     }
 
     create() {
-        this.add.sprite(300, 300, 'fondo');
+        this.add.sprite(300, 300, 'fondo');    
+        this.sound.play('sonido');    
 
         const opcionNave = this.add.zone(125, 125, 300, 300);
         opcionNave.setOrigin(0);
         opcionNave.setInteractive();
         opcionNave.once('pointerdown', () => this.opcionPulsada('nave'));
-        this.add.graphics().lineStyle(2, 0xff000).strokeRectShape(opcionNave);
+        // this.add.graphics().lineStyle(2, 0xff000).strokeRectShape(opcionNave);
 
         const opcionTierra = this.add.zone(400, 350, 350, 250);
         opcionTierra.setOrigin(0);
         opcionTierra.setInteractive();
         opcionTierra.once('pointerdown', () => this.opcionPulsada('tierra'));
-        this.add.graphics().lineStyle(2, 0xff000).strokeRectShape(opcionTierra);
+        // this.add.graphics().lineStyle(2, 0xff000).strokeRectShape(opcionTierra);
+
+        this.input.setDefaultCursor('url(../cursor/cursor.cur), pointer');
+
     }
 
     update() {
@@ -25,7 +31,59 @@ class Escena extends Phaser.Scene {
     }
 
     opcionPulsada(opcion) {
-        alert(opcion);
+        if(opcion == 'nave') {
+            this.scene.start('naveScene');
+        }
+
+        if(opcion == 'tierra') {
+            this.scene.start('tierraScene');
+        }
+    }
+}
+
+class EscenaTierra extends Phaser.Scene {
+
+    constructor() {
+        super('tierraScene');
+    }
+
+    preload() {
+        this.load.image('earth', '../Images/earthplanet.png');
+    }
+
+    create() {
+        this.add.sprite(300, 300, 'earth');
+    }
+
+}
+
+class EscenaLuna extends Phaser.Scene {
+
+    constructor() {
+        super('lunaScene');
+    }
+
+    preload() {
+        this.load.image('luna', '../Images/moon.png');
+    }
+
+    create() {
+        this.add.sprite(300, 300, 'luna');
+    }
+}
+
+class EscenaNave extends Phaser.Scene {
+
+    constructor() {
+        super('naveScene');
+    }
+
+    preload() {
+        this.load.image('nave', '../Images/spacecraft.png');
+    }
+
+    create() {
+        this.add.sprite(300, 300, 'nave');
     }
 }
 
@@ -33,7 +91,7 @@ const config = {
     type: Phaser.AUTO, /* Motor de renderizado es automatico */
     width: 960,
     height: 640,
-    scene: Escena, /* El objeto solo tiene una sola Escena */
+    scene: [Escena, EscenaTierra, EscenaLuna, EscenaNave], /* El objeto solo tiene una sola Escena */
     scale: {
         mode: Phaser.Scale.FIT /* Ancho y alto se redimensiona respetando la proporcion definida anteriormente */
     }
